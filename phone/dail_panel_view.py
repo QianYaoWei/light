1567c9c26590df5259a59cbdb7b83496640ddef1
weiqy <xxx@xxx.com>
2017-09-19 18:20:42 +0800

    add phone

---
+++ phone/dail_panel_view.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from common import *
+import util
+import util.win as win
+
+
+class DailPanelView(win.View):
+    def __init__(self, stdscr, sch=None):
+        super(DailPanelView, self).__init__(stdscr, win.DailPanelScr_id, sch)
+
+
+def main(stdscr):
+    view = DailPanelView(stdscr)
+
+    reciever = util.CommandReciever()
+    view.Init(reciever)
+    reciever.start()
+
+    sender = util.CommandSender(stdscr)
+    sender.start()
+
+    view.Sched.run()
+
+    reciever.join()
+    sender.join()
+
+if __name__ == "__main__":
+    curses.wrapper(main)
